cbc95a3180b509e2b868f228cef8c3848a469bd3
BlurryFlurry <chan@elakiri.com>
2015-12-09 13:57:05 +0530

    config moved to html mount dir

---
+++ html/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config['db']['host'] = 'localhost';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'ekdevusr';
+$config['db']['password'] = 'ekdevpw';
+$config['db']['dbname'] = 'ekdev_test';
+$config['debug'] = 'enable';
+
+
+$config['superAdmins'] = '1';
