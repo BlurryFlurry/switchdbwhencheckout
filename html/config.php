6353e1ddf45adb346f057ff6c641e6b7b33f451e
BlurryFlurry <chan@elakiri.com>
2015-12-16 09:06:03 +0530

    auto set database settings by detecting the git branch and use it as a prefix

--- html/config.php
+++ html/config.php
@@ -1,11 +1,24 @@
 <?php
 
+$path_to_git_head = '../.git/HEAD';
+if (file_exists($path_to_git_head)) {
+	if ($git_head = file($path_to_git_head, FILE_USE_INCLUDE_PATH)) {
+ 	$git_head = $git_head[0];
+ 	$git_head_exploded = explode("/", $git_head);
+ 	$git_branch = rtrim($git_head_exploded[2]);
+ 	$original_dbname = 'ekdev';
+ 	$dbnameWithBranchPrefix = $original_dbname . "_" . $git_branch;
+	}
+}
+
 $config['db']['host'] = 'localhost';
 $config['db']['port'] = '3306';
 $config['db']['username'] = 'ekdevusr';
 $config['db']['password'] = 'ekdevpw';
-$config['db']['dbname'] = 'ekdev_test';
+$config['db']['dbname'] = isset($dbnameWithBranchPrefix) ? $dbnameWithBranchPrefix : 'ekdev'; 
 $config['debug'] = 'enable';
 
 
 $config['superAdmins'] = '1';
+
+echo $config['db']['dbname'];
